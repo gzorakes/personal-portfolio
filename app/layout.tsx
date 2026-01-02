894c50590bec6938596a32a3b3672f80367a12ea
zorakisgeorge <zorakisgeorge@gmail.com>
2026-01-02 11:55:13 +0200

    project setup/shadcn/custom navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
+import { ThemeProvider } from "../components/ui/theme-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/web/navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -23,11 +21,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/web/navbar.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import Image from "next/image";
+import { ThemeToggle } from "./theme-toggle";
+import Link from "next/link";
+import { Button, buttonVariants } from "../ui/button";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+const navItems = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="max-w-7xl mx-auto p-4 mb-10">
+      <div className="flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo-light.svg"
+            width={160}
+            height={49}
+            loading="eager"
+            alt="George Zorakis's logo"
+            className="dark:hidden"
+          />
+          <Image
+            src="/logo-dark.svg"
+            width={160}
+            height={49}
+            loading="eager"
+            alt="George Zorakis's logo"
+            className="hidden dark:block"
+          />
+        </Link>
+
+        <nav>
+          <ul className="hidden md:flex items-center gap-10">
+            {navItems.map((link) => {
+              const isActive =
+                pathname === link.href ||
+                (pathname.startsWith(link.href) && link.href !== "/");
+              return (
+                <li key={link.href}>
+                  <Link
+                    className={cn(
+                      "transition-colors duration-300 hover:text-foreground/80",
+                      isActive
+                        ? buttonVariants({
+                            className: "text-md",
+                            variant: "secondary",
+                          })
+                        : "text-foreground/60"
+                    )}
+                    aria-current={isActive ? "page" : undefined}
+                    href={link.href}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <div className="flex items-center gap-2">
+          <ThemeToggle />
+          <Link
+            href="/contact"
+            className={cn(buttonVariants(), "hidden md:block")}
+          >
+            LET&apos;S TALK
+          </Link>
+          <Button
+            variant="outline"
+            size="icon"
+            className="md:hidden"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          >
+            {isMobileMenuOpen ? <X /> : <Menu />}
+          </Button>
+        </div>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="bg-secondary rounded-md mt-4 p-4 md:hidden">
+          <ul className="flex flex-col gap-6">
+            {navItems.map((link) => {
+              const isActive =
+                pathname === link.href ||
+                (pathname.startsWith(link.href) && link.href !== "/");
+              return (
+                <li key={link.href}>
+                  <Link
+                    onClick={() => setIsMobileMenuOpen(false)}
+                    className={cn(
+                      "transition-colors duration-300 hover:text-foreground/80 block",
+                      isActive
+                        ? "bg-accent rounded-md p-2"
+                        : "text-foreground/60"
+                    )}
+                    aria-current={isActive ? "page" : undefined}
+                    href={link.href}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+}
